30eeaf28ae4f9958888e8cc0e3cb8ab8b85f8040
Nikita Borovkov <borovckov@yandex.ru>
2021-09-06 00:54:13 +0300

    added TODO.js

---
+++ TODO.js
@@ -0,0 +1,10 @@
+//TODO `ADD search forms to each page`
+//TODO `ADD queries`
+//TODO `ADD lazy loading`
+//TODO `add character opening in separate window`
+//TODO `ADD info for quites, deaths and episodes open in modal window or in card`
+//TODO `ADD preloader`
+//TODO `ADD home page with carousel with random shit and some more info`
+//TODO `REMOVE CODE repeat in components (title, subtitle)`
+//TODO `ADD footer with (Characters: 62; Episodes: 102; Quotes: 70; Death Count: 271)`
+//TODO `ADD PWA (-ish)`
