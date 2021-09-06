20d2193a4ae73c4540a010abf99172949f28ce6d
Nickborovkov <borovckov@yandex.ru>
2021-09-06 17:32:09 +0300

    added vuelidate

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import store from './store'
 import axios from "axios";
 import vuetify from './plugins/vuetify'
 import router from "./routes";
+
+
+Vue.use(Vuelidate)
 
 Vue.config.productionTip = false
 
