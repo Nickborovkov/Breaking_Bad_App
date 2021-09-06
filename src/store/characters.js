d309e9fb392e6251853b9bf8f21e353abb7627e3
Nickborovkov <borovckov@yandex.ru>
2021-09-06 17:35:52 +0300

    added search characters form, when created component loads 3 random characters

--- src/store/characters.js
+++ src/store/characters.js
@@ -1,23 +1,38 @@
-import {breakingBadApi} from "../serverRequests/serverRequests";
+import {charactersAPI} from "../serverRequests/serverRequests";
 
 export default {
     state: {
-        characters: []
+        characters: [],
+        specificCharacter: null,
     },
     mutations: {
         setCharacters (state, characters) {
             state.characters = characters
+        },
+        setSpecificCharacter (state, character) {
+            state.specificCharacter = character
         }
     },
     actions: {
-        async getCharacters ({commit}) {
-            const response = await breakingBadApi.requestCharacters()
+        async getRandomCharacters ({commit}, count) {
+            const response = await charactersAPI.requestRandomCharacters(count)
             commit(`setCharacters`, response.data)
+        },
+        async getCharactersByName ({commit}, characters) {
+            const response = await charactersAPI.requestCharactersByName(characters)
+            commit(`setCharacters`, response.data)
+        },
+        async getCharactersById ({commit}, id) {
+            const response = await charactersAPI.requestCharactersByid(id)
+            commit(`setSpecificCharacter`, response.data[0])
         }
     },
     getters: {
         getCharacters (state) {
             return state.characters
+        },
+        getSpecificCharacter (state) {
+            return state.specificCharacter
         }
     },
 }
