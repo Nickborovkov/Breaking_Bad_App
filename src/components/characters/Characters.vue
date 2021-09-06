3e7478b8884c312f3956999fe5df6dec4a123bdc
Nickborovkov <borovckov@yandex.ru>
2021-09-06 17:35:01 +0300

    added component for showing chosen character

--- src/components/characters/Characters.vue
+++ src/components/characters/Characters.vue
@@ -1,11 +1,29 @@
 <template>
     <v-container>
-        <h1>CHARACTERS</h1>
-        <v-layout wrap>
-            <v-flex xs12 sm3
+        <h1 class="text-center mt-16 mb-16">CHARACTERS</h1>
+
+        <v-layout justify-center>
+            <v-flex xs12 sm6 lg6  class="mt-16 mb-16">
+
+                <appSearch></appSearch>
+
+                <v-container>
+                    <v-text-field outlined v-model.number="random" clearable color="green"
+                                  label="Or choose amount of random characters"></v-text-field>
+                    <v-btn color="green" @click="getRandom(random)" >
+                        Get random
+                    </v-btn>
+                </v-container>
+
+
+            </v-flex>
+        </v-layout>
+
+        <v-layout wrap justify-center>
+            <v-flex xs12 sm3 lg4
                     :key="item.char_id"
                     v-for="item in charactersList">
-                <v-card class="ma-3">
+                <v-card class="ma-3" :to="`characters/character/${item.char_id}`">
                     <v-img
                             :src="item.img"
                             height="500"
@@ -15,81 +33,6 @@
                         {{item.name}}
                     </v-card-title>
 
-                    <v-card-actions>
-                        <v-btn
-                                color="orange lighten-2"
-                                text
-                        >
-                            Explore
-                        </v-btn>
-
-                        <v-spacer></v-spacer>
-
-                        <v-btn
-                                icon
-                                @click="show = !show"
-                        >
-                            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-                        </v-btn>
-                    </v-card-actions>
-
-                    <v-expand-transition>
-                        <div v-show="show">
-                            <v-divider></v-divider>
-
-                            <v-card-text>
-
-                                <v-card-title>
-                                    Nickname
-                                </v-card-title>
-
-                                <v-card-subtitle>
-                                    {{item.nickname}}
-                                </v-card-subtitle>
-
-                                <v-card-title>
-                                    Status:
-                                </v-card-title>
-
-                                <v-card-subtitle>
-                                    {{item.status}}
-                                </v-card-subtitle>
-
-                                <v-card-title>
-                                    Birth date:
-                                </v-card-title>
-
-                                <v-card-subtitle>
-                                    {{item.birthday}}
-                                </v-card-subtitle>
-
-                                <v-card-title>
-                                    Occupation:
-                                </v-card-title>
-
-                                <v-card-subtitle>
-                                    <p :key="occupation" v-for="occupation in item.occupation">{{occupation}}</p>
-                                </v-card-subtitle>
-
-                                <v-card-title>
-                                    Appearance:
-                                </v-card-title>
-
-                                <v-card-subtitle>
-                                    <p :key="appearance" v-for="appearance in item.appearance">{{appearance}}</p>
-                                </v-card-subtitle>
-
-                                <v-card-title>
-                                    Actor:
-                                </v-card-title>
-
-                                <v-card-subtitle>
-                                    {{item.portrayed}}
-                                </v-card-subtitle>
-
-                            </v-card-text>
-                        </div>
-                    </v-expand-transition>
                 </v-card>
             </v-flex>
         </v-layout>
@@ -99,20 +42,27 @@
 </template>
 
 <script>
+    import CharacterSearch from "./searchForm/CharacterSearch";
 
     export default {
-        data () {
-            return {
-                show: false
-            }
-        },
+        data: () => ({
+            random: null,
+        }),
         computed: {
             charactersList () {
                 return this.$store.getters.getCharacters
-            }
+            },
         },
         created() {
-            this.$store.dispatch(`getCharacters`)
+            this.$store.dispatch(`getRandomCharacters`, 3)
+        },
+        methods: {
+            getRandom (value) {
+                this.$store.dispatch(`getRandomCharacters`, value)
+            }
+        },
+        components: {
+            appSearch: CharacterSearch
         }
     }
 
