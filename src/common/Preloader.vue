b2ea07fbbdf83ee6c0d8ec4ecd5097334d7ce09e
Nickborovkov <borovckov@yandex.ru>
2021-09-07 16:57:45 +0300

    added preloader

---
+++ src/common/Preloader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="text-center mt-16">
+        <v-progress-circular
+                indeterminate
+                :size="200"
+                :width="10"
+                color="green"
+        ></v-progress-circular>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
