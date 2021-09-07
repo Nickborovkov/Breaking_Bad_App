e2dd19ac7185ad8ac8d3f0f8ae8574cf2c750cb4
Nickborovkov <borovckov@yandex.ru>
2021-09-07 16:56:20 +0300

    added footer component

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-footer color="green">
+        <v-layout>
+
+            <v-flex>
+                <a href="https://www.breakingbadapi.com/documentation">API provided by TheBreaking Bad API</a>
+            </v-flex>
+
+            <v-flex>
+
+            </v-flex>
+
+            <v-flex>
+                Made by Nick Borovkov
+            </v-flex>
+
+        </v-layout>
+    </v-footer>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
