9886a9ab7c023d46ebbe86732c7f1e3a8ca45fc9
Nickborovkov <borovckov@yandex.ru>
2021-09-07 16:57:18 +0300

    separate store module for common actions

--- src/store/common.js
+++ src/store/common.js
@@ -1,1 +1,19 @@
-export
+export default {
+    state: {
+        error: null,
+        isLoading: false
+    },
+    mutations: {
+        setError (state, error) {
+            state.error = error
+        },
+        setLoading (state, isLoading) {
+            state.isLoading = isLoading
+        }
+    },
+    getters: {
+        getLoading (state) {
+            return state.isLoading
+        }
+    },
+}
