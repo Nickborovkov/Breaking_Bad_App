48a7642b13423ceab33f5cce4494e20d5469254a
Nikita Borovkov <borovckov@yandex.ru>
2021-09-13 02:50:40 +0300

    added error page

---
+++ src/common/page404/Page404.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container>
+        <v-layout justify-center>
+            <v-btn
+                    color="green"
+                    :to="`/`"
+            >
+                <v-icon left>mdi-arrow-left</v-icon>
+                Back to Home page
+            </v-btn>
+        </v-layout>
+        <h1 class="text-center mt-16 mb-16 text-uppercase">Page not found</h1>
+
+    </v-container>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
