02b5b5462c36ac76a3ae156ad2d1994181071479
Nikita Borovkov <borovckov@yandex.ru>
2021-09-13 02:50:58 +0300

    update TODO.js

--- TODO.js
+++ TODO.js
@@ -1,12 +1,12 @@
 //TODO `ADD search forms to each page`
 //TODO `ADD queries`
 //TODO `ADD lazy loading`
-//TODO `add character opening in separate window`
 //TODO `ADD info for quites, deaths and episodes open in modal window or in card`
-//TODO `ADD preloader`
 //TODO `ADD home page with carousel with random shit and some more info`
 //TODO `REMOVE CODE repeat in components (title, subtitle)`
-//TODO `ADD footer with (Characters: 62; Episodes: 102; Quotes: 70; Death Count: 271)`
 //TODO `ADD PWA (-ish)`
-//TODO `404 page`
-//TODO `get back from character to characters component (with no parent component rebuild)`
+//TODO `get back from characterById to characters component (with no parent component rebuild)`
+//TODO `fix search form and add validations`
+//TODO `check adaptivity`
+//TODO `finish quotes and deaths pages`
+//TODO `deploy to github pages`
