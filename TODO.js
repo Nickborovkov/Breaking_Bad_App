91e04031fb8240e7cef7a49708bc5444c5615343
Nikita Borovkov <borovckov@yandex.ru>
2021-09-18 02:51:27 +0300

    update TODO.js

--- TODO.js
+++ TODO.js
@@ -3,13 +3,3 @@
 //TODO `????add links in rendered components`
 //TODO `main menu`
 //TODO `REMOVE CODE repeat in components (title, subtitle)`
-
-
-//TODO `ADD PWA (-ish)`
-//TODO `< error in console`
-//TODO `deploy to github pages`
-//TODO `add logo`
-
-//TODO `validation works incorrectly with capital letters in english`
-//TODO `validate on enter on mobile`
-//TODO `hash mode instead history mode`
