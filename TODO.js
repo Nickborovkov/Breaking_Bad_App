d84b253b845ffdfebdfd7992debb6bc1f4b3362d
Nikita Borovkov <borovckov@yandex.ru>
2021-09-20 02:42:11 +0300

    update TODO.js

--- TODO.js
+++ TODO.js
@@ -1,5 +1,6 @@
-//TODO `errors handling`
 //TODO `????get back from characterById to characters component (with no parent component rebuild)`
 //TODO `????add links in rendered components`
+//TODO `errors handling`
 //TODO `main menu`
-//TODO `REMOVE CODE repeat in components (title, subtitle)`
+//TODO `replace code repeating in Character by Id component`
+//TODO `comment the code, final refactor, final deploy`
